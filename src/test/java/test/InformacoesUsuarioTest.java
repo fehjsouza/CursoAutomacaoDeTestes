23a6be9c16b10efdad3bba0599d64480e988ff15
fehjsouza <feh_jsouza@hotmail.com>
2021-09-17 13:36:48 -0300

    First Commit

---
+++ src/test/java/test/InformacoesUsuarioTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class InformacoesUsuarioTest {
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+        // Abrindo Navegador
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Driver\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        // navegando para a página do Task!
+        navegador.get("http://www.juliodelima.com.br/taskit");
+
+        // Clicar no link que possui o texto "Sign in"
+        navegador.findElement(By.linkText("Sign in")).click();
+
+        // Identificado o formulário de login
+        WebElement formularioSignInBox = navegador.findElement(By.id("signinbox"));
+
+        // Digitar no campo name "login" que está dentro do formulario de id "signinbox" o texto "julio0001"
+        formularioSignInBox.findElement(By.name("login")).sendKeys("julio0001");
+
+        // Digitar no campo name "password" que está dentro do formulario de id "signinbox" o texto "123456"
+        formularioSignInBox.findElement(By.name("password")).sendKeys("123456");
+
+        // Clicar no link com o texto "SIGN IN"
+        navegador.findElement(By.linkText("SIGN IN")).click();
+
+        // Validar que dentro do elemento class "me" está do texto "Hi, Julio"
+        WebElement me = navegador.findElement(By.className("me"));
+        String textoNoElementoMe = me.getText();
+        assertEquals("Hi, Julio", textoNoElementoMe);
+
+        // Fechar o navegador
+        navegador.quit();
+
+
+    }
+}
